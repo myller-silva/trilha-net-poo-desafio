9624dbda016b984586b1eb90f3b867d99525cd5f
mylle <myllersilva1310@gmail.com>
2023-07-13 21:48:57 -0300

    update

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private string Memoria { get; set; }
+
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, string memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria; 
         }
 
         public void Ligar()
